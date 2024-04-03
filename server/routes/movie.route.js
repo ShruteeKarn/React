a69382b225a7a61434a5011efe1db624dd02470a
Shivam Mishra <shivamkm123@gmail.com>
2024-04-04 02:25:28 +0530

    Updated

--- server/routes/movie.route.js
+++ server/routes/movie.route.js
@@ -1,73 +1,62 @@
-const express=require("express");
-const mongoose=require("mongoose");
+const express = require("express");
+const mongoose = require("mongoose");
 const { Authentication } = require("../middlewares/Authentication");
 
 const MylistModel = require("../model/ListModel");
 const MovieModel = require("../model/MovieModel");
-const movieRoute=express.Router();
+const movieRoute = express.Router();
 
-
-//get all series/movie/netflix original data
-
-movieRoute.get("/:category",async(req,res)=>{
-    const category=req.params.category||"movie"
-    try{
-        const allMovieData=await MovieModel.find({category})
-        res.status(200).send({data:allMovieData})
-
+// get all series/movie/netflix original data
+movieRoute.get("/:category", async (req, res) => {
+    const category = req.params.category || "movie";
+    try {
+        const allMovieData = await MovieModel.find({ category });
+        res.status(200).send({ data: allMovieData });
+    } catch (err) {
+        res.status(404).send({ msg: err.message });
     }
-    catch(err){
-        res.status(404).send({msg:err.message})
-    }
-})
-movieRoute.get("/",async(req,res)=>{
-   
-    try{
-        const allMovieData=await MovieModel.find()
-        res.status(200).send({data:allMovieData})
+});
 
+movieRoute.get("/", async (req, res) => {
+    try {
+        const allMovieData = await MovieModel.find();
+        res.status(200).send({ data: allMovieData });
+    } catch (err) {
+        res.status(404).send({ msg: err.message });
     }
-    catch(err){
-        res.status(404).send({msg:err.message})
-    }
-})
+});
 
-
-movieRoute.post("/addlist/:id",Authentication,async(req,res)=>{
-    const movieId=req.params.id;
-    const userid=req.body.userid;
-    try{
-        const newMovie=await MylistModel({movie:movieId,user:userid})
+movieRoute.post("/addlist/:id", Authentication, async (req, res) => {
+    const movieId = req.params.id;
+    const userId = req.body.userid;
+    try {
+        const newMovie = await MylistModel({ movie: movieId, user: userId });
         await newMovie.save();
-        res.status(200).send({msg:"added to list successfully"})
-    }catch(err){
-        res.status(404).send({msg:err.message})
+        res.status(200).send({ msg: "Added to list successfully" });
+    } catch (err) {
+        res.status(404).send({ msg: err.message });
     }
-})
+});
 
-movieRoute.get("/list/all",Authentication,async(req,res)=>{
-    const userId=req.body.userid;
-    console.log(userId)
-    try{
-        const allList=await MylistModel.find({user:userId}).populate("movie");
-        res.status(200).send({data:allList})
-
-    }catch(err){
-        res.status(404).send({msg:err.message})
+movieRoute.get("/list/all", Authentication, async (req, res) => {
+    const userId = req.body.userid;
+    try {
+        const allList = await MylistModel.find({ user: userId }).populate("movie");
+        res.status(200).send({ data: allList });
+    } catch (err) {
+        res.status(404).send({ msg: err.message });
     }
-})
+});
 
-movieRoute.delete("/list/:id",Authentication,async(req,res)=>{
-    const listId=req.params.id;
-    const userId=req.body.userid
-
-    try{
-        const movieTobeDeleted=await MylistModel.findOneAndDelete({user:userId,_id:listId})
-        res.status(200).send({msg:"Removed from successfully"})
-
-    }catch(err){
-        res.status(404).send({msg:err.message})
+movieRoute.delete("/list/:id", Authentication, async (req, res) => {
+    const listId = req.params.id;
+    const userId = req.body.userid;
+    try {
+        const movieTobeDeleted = await MylistModel.findOneAndDelete({ user: userId, _id: listId });
+        res.status(200).send({ msg: "Removed successfully" });
+    } catch (err) {
+        res.status(404).send({ msg: err.message });
     }
-})
+});
 
-module.exports={movieRoute}
+module.exports = { movieRoute };
